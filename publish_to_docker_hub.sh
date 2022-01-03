80d475181a122e57367e0fcaddbb4db4f20244f4
svenwal <sven@svenwal.de>
2022-01-03 10:47:11 +0100

    Added building script for AMD64 and ARM64

---
+++ publish_to_docker_hub.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+if [ $# -lt "1" ]
+then
+  echo "USAGE publish_to_docker_hub.sh TAG"
+  echo ""
+  exit
+fi  
+
+tag=$1
+
+echo "*** Publishing current state to Docker Hub with the tags $tag and latest"
+echo "--- 1. Building current version $tag without using caches"
+docker buildx build --push --platform linux/amd64,linux/arm64 --no-cache -t svenwal/jsonplaceholder:$tag .
+echo "--- 2. Building current version as latest"
+docker buildx build --push --platform linux/amd64,linux/arm64 -t svenwal/jsonplaceholder:latest .
+
